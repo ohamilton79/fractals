1139caa915d4c44ff36123e1673abb558aa2e5e0
ohamilton79 <ohamilton0079@gmail.com>
2021-05-22 22:01:25 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# mandelbrot-generator
+This Python script uses the NumPy and Matplotlib libraries to draw the Mandlebrot set. The resulting image has a final name corresponding to the values used to generate it (the x and y offsets, as well as the magnification)
+
+# Key features:
+Provides options to create images with different magnifications and offsets in the x and y directions.
+
+Although drawn as a square image with a fixed aspect ratio, the resolution of the image can be modified with the 'step' variable in the program, measured in pixels. 
+
+You can use the grayscale flag to choose between a colour or black and white image.
---
+++ sierpinski/straight_sierpinski.py
@@ -0,0 +1,51 @@
+#ohamilton79
+#Straight-line Sierpinksi curve drawing
+#22/05/2021
+import turtle
+
+def drawZig(t, size):
+    t.left(90)
+    t.forward(size)
+    t.left(90)
+    t.forward(size)
+
+def drawZag(t, size):
+    t.right(90)
+    t.forward(size)
+    t.right(90)
+    t.forward(size)
+    t.left(90)
+    t.forward(size)
+    
+def zig(t, size, n):
+    if n == 1:
+        drawZig(t, size)
+
+    else:
+        zig(t, size, n-1)
+        zag(t, size, n-1)
+        zig(t, size, n-1)
+        zag(t, size, n-1)
+
+def zag(t, size, n):
+    if n == 1:
+        drawZag(t, size)
+
+    else:
+        zag(t, size, n-1)
+        zag(t, size, n-1)
+        zig(t, size, n-1)
+        zag(t, size, n-1)
+
+def sierpinski(t, size, n):
+    zig(t, size, n)
+    zig(t, size, n)
+
+#Initialise
+t = turtle.Turtle()
+t.speed('fastest')
+t.hideturtle()
+size = 30
+
+#Test data
+sierpinski(t, size, 3)
